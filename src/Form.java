6bc778c72721e38a441dd0b735769679964762a8
Zafira Candra <Zafira Candra@DESKTOP-9FID3PD>
2017-03-21 22:39:50 +0700

    commit3

--- src/Form.java
+++ src/Form.java
@@ -1,6 +1,12 @@
 
+import static java.lang.Math.toIntExact;
+import java.text.DateFormat;
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import uts.KoneksiDB;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,12 +18,12 @@ import javax.swing.JOptionPane;
  *
  * @author Zafira Candra
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class Form extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public NewJFrame() {
+    public Form() {
         initComponents();
     }
 
@@ -41,8 +47,8 @@ public class NewJFrame extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable = new javax.swing.JTable();
-        harga = new javax.swing.JTextField();
+        tbRental = new javax.swing.JTable();
+        lamaPinjam = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
         tglKembali = new com.toedter.calendar.JDateChooser();
         tglPinjam = new com.toedter.calendar.JDateChooser();
@@ -51,6 +57,9 @@ public class NewJFrame extends javax.swing.JFrame {
         btnSave = new javax.swing.JButton();
         btnClear = new javax.swing.JButton();
         btnDelete = new javax.swing.JButton();
+        hitungHari = new javax.swing.JButton();
+        harga1 = new javax.swing.JTextField();
+        jLabel10 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -120,7 +129,7 @@ public class NewJFrame extends javax.swing.JFrame {
         jPanel2.add(jLabel8);
         jLabel8.setBounds(10, 310, 120, 15);
 
-        jTable.setModel(new javax.swing.table.DefaultTableModel(
+        tbRental.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null, null, null},
                 {null, null, null, null, null, null, null},
@@ -131,17 +140,17 @@ public class NewJFrame extends javax.swing.JFrame {
                 "Nama", "Alamat", "No Sruk", "No Pol", "Tanggal Pinjam", "Tanggal Kembali", "Harga"
             }
         ));
-        jScrollPane1.setViewportView(jTable);
+        jScrollPane1.setViewportView(tbRental);
 
         jPanel2.add(jScrollPane1);
         jScrollPane1.setBounds(210, 80, 520, 350);
-        jPanel2.add(harga);
-        harga.setBounds(10, 390, 170, 30);
+        jPanel2.add(lamaPinjam);
+        lamaPinjam.setBounds(10, 460, 170, 30);
 
         jLabel9.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel9.setText("Harga");
+        jLabel9.setText("Lama Hari");
         jPanel2.add(jLabel9);
-        jLabel9.setBounds(10, 370, 120, 15);
+        jLabel9.setBounds(10, 440, 120, 15);
         jPanel2.add(tglKembali);
         tglKembali.setBounds(10, 330, 170, 30);
         jPanel2.add(tglPinjam);
@@ -150,6 +159,11 @@ public class NewJFrame extends javax.swing.JFrame {
         jPanel3.setLayout(null);
 
         btnPrint.setText("Print");
+        btnPrint.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPrintActionPerformed(evt);
+            }
+        });
         jPanel3.add(btnPrint);
         btnPrint.setBounds(410, 10, 100, 40);
 
@@ -163,18 +177,44 @@ public class NewJFrame extends javax.swing.JFrame {
         btnSave.setBounds(10, 10, 110, 40);
 
         btnClear.setText("Clear");
+        btnClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClearActionPerformed(evt);
+            }
+        });
         jPanel3.add(btnClear);
         btnClear.setBounds(140, 10, 110, 40);
 
         btnDelete.setText("Delete");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
         jPanel3.add(btnDelete);
         btnDelete.setBounds(280, 10, 100, 40);
 
         jPanel2.add(jPanel3);
         jPanel3.setBounds(210, 10, 520, 60);
 
+        hitungHari.setText("Hitung Hari");
+        hitungHari.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                hitungHariActionPerformed(evt);
+            }
+        });
+        jPanel2.add(hitungHari);
+        hitungHari.setBounds(10, 500, 100, 40);
+        jPanel2.add(harga1);
+        harga1.setBounds(10, 390, 170, 30);
+
+        jLabel10.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel10.setText("Harga");
+        jPanel2.add(jLabel10);
+        jLabel10.setBounds(10, 370, 120, 15);
+
         getContentPane().add(jPanel2);
-        jPanel2.setBounds(10, 100, 750, 440);
+        jPanel2.setBounds(10, 100, 750, 550);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -182,15 +222,15 @@ public class NewJFrame extends javax.swing.JFrame {
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
         // TODO add your handling code here:
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String pinjam = dateFormat.format(tPinjam.getDate());
-        String kembali = dateFormat.format(tKembali.getDate());
+        String pinjam = dateFormat.format(tglPinjam.getDate());
+        String kembali = dateFormat.format(tglKembali.getDate());
         
        
-        if("".equals(tNama.getText()) || "".equals(tAlamat.getText()) || "".equals(tStruk.getText()) || pinjam.equals("") || kembali.equals("") || "".equals(tPol.getText()) ||  "".equals(tHarga.getText())   ){
+        if("".equals(namaPeminjam.getText()) || "".equals(alamatPeminjam.getText()) || "".equals(noStruk.getText()) || pinjam.equals("") || kembali.equals("") || "".equals(noPol.getText()) ||  "".equals(harga1.getText())   ){
             JOptionPane.showMessageDialog(this, "Harap Lengkapi Data", "Error", JOptionPane.WARNING_MESSAGE);
         } else {
 
-            String SQL = "INSERT INTO data VALUES ('"+namaPeminjam.getText()+"','"+alamatPeminjam.getText()+"','"+noStruk.getText()+"','"+"','"+"','"+noPol.getText()+"','"+pinjam+"','"+kembali+"','"+harga.getText()+"')";
+            String SQL = "INSERT INTO data VALUES ('"+namaPeminjam.getText()+"','"+alamatPeminjam.getText()+"','"+noStruk.getText()+"','"+"','"+"','"+noPol.getText()+"','"+pinjam+"','"+kembali+"','"+lamaPinjam.getText()+"')";
             int status = KoneksiDB.execute(SQL);
             if(status == 1){
                 JOptionPane.showMessageDialog(this, "Data berhasil ditambahkan", "Sukses", JOptionPane.INFORMATION_MESSAGE);
@@ -204,6 +244,81 @@ public class NewJFrame extends javax.swing.JFrame {
     private void namaPeminjamActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_namaPeminjamActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_namaPeminjamActionPerformed
+
+    private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearActionPerformed
+        // TODO add your handling code here:
+        namaPeminjam.setText("");
+        alamatPeminjam.setText("");
+        noStruk.setText("");
+        noPol.setText("");
+        tglPinjam.setDate(null);
+        tglKembali.setDate(null);
+        lamaPinjam.setText("");
+    }//GEN-LAST:event_btnClearActionPerformed
+
+    private void hitungHariActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hitungHariActionPerformed
+        // TODO add your handling code here:
+        Date Pinjam = tglPinjam.getDate();
+        Date Kembali = tglKembali.getDate();
+        long startTime = Pinjam.getTime();
+        long endTime = Kembali.getTime();
+        
+        long sewaTime = endTime - startTime;
+        long bedaHari = sewaTime/(1000 * 60 * 60 * 24);
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        
+        lamaPinjam.setText(" "+bedaHari+" hari");
+        
+        int lama = toIntExact(bedaHari);
+        int total = lama * 20000;
+        String harga = Integer.toString(total);
+        
+        harga1.setText(harga);
+    }//GEN-LAST:event_hitungHariActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        // TODO add your handling code here:
+        int baris = tbRental.getSelectedRow();
+        int confirm = JOptionPane.showConfirmDialog(null,"Anda yakin ingin menghapus data?","Confirmation", JOptionPane.YES_NO_OPTION);
+        if (confirm == 0){
+            if(baris != -1){
+                String struk = tbRental.getValueAt(baris, 2).toString();
+                String SQL = "DELETE FROM db_uts WHERE struk='"+struk+"'";
+                int status = KoneksiDB.execute(SQL);
+                
+                if(status == 1){
+                    JOptionPane.showMessageDialog(this, "Data berhasil dihapus", "Sukses", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Data gagal dihapus", "Gagal", JOptionPane.WARNING_MESSAGE);
+                }
+            } else {
+               JOptionPane.showMessageDialog(this, "Pilih Baris Data Terlebih Dahulu", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+
+    }                                        
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // TODO add your handling code here:
+        MessageFormat header = new MessageFormat("Biodata Siswa SMK Telkom Malang");
+        MessageFormat footer = new MessageFormat("Page {0,number,integer}   ");
+        try{
+            tbRental.print(JTable.PrintMode.FIT_WIDTH, header, footer, true, null, true, null);
+        } catch (java.awt.print.PrinterException e) {
+            System.err.format("Cannot print %s%n", e.getMessage());
+        }
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    private void btnPrintActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrintActionPerformed
+        // TODO add your handling code here:
+        MessageFormat header = new MessageFormat("Biodata Siswa SMK Telkom Malang");
+        MessageFormat footer = new MessageFormat("Page {0,number,integer}   ");
+        try{
+            tbRental.print(JTable.PrintMode.FIT_WIDTH, header, footer, true, null, true, null);
+        } catch (java.awt.print.PrinterException e) {
+            System.err.format("Cannot print %s%n", e.getMessage());
+        }
+    }//GEN-LAST:event_btnPrintActionPerformed
 
     /**
      * @param args the command line arguments
@@ -222,20 +337,21 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new Form().setVisible(true);
             }
         });
     }
@@ -246,8 +362,10 @@ public class NewJFrame extends javax.swing.JFrame {
     private javax.swing.JButton btnDelete;
     private javax.swing.JButton btnPrint;
     private javax.swing.JButton btnSave;
-    private javax.swing.JTextField harga;
+    private javax.swing.JTextField harga1;
+    private javax.swing.JButton hitungHari;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -260,11 +378,12 @@ public class NewJFrame extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable;
     private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField lamaPinjam;
     private javax.swing.JTextField namaPeminjam;
     private javax.swing.JTextField noPol;
     private javax.swing.JTextField noStruk;
+    private javax.swing.JTable tbRental;
     private com.toedter.calendar.JDateChooser tglKembali;
     private com.toedter.calendar.JDateChooser tglPinjam;
     // End of variables declaration//GEN-END:variables
